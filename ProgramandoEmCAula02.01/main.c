5e2cdc300527d1365fb95e308259d0a51457bdb2
marcosnava <nava.marcos@gmail.com>
2019-04-11 11:53:06 -0300

    Aula 2

---
+++ ProgramandoEmCAula02.01/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int idade; // cria uma variável inteira
+    char sexo; // variavel char (M/F)
+    float raio; // variavel ponto flutuante
+    double total; // variavel ponto flutuante grande
+}
---
+++ ProgramandoEmCAula02.02/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int anoAtual = 2019; // criando variável com conteúdo
+    int anoNascimento = 1999;
+    int idade; // variável sem conteúdo, com lixo
+
+    idade = anoAtual - anoNascimento; // atribuindo um cálculo
+    return 0;
+}
